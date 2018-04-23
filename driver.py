2cbe66163796e24be84ec1d500e256bc45008dbd
Alex Maltsev <alexandr@idisys.iae.nsk.su>
2018-04-23 16:03:54 +0700

    driver: refactoring

--- driver.py
+++ driver.py
@@ -1,8 +1,8 @@
 import serial
-import time
-import os
+
 import rospy
 from std_msgs.msg import String
+
 
 class Driver:
 
@@ -10,22 +10,22 @@ class Driver:
         self.serial = serial.Serial(device, baud, timeout=1)
         rospy.Subscriber("/robot/motor", String, self.motor_callback)
 
-    def send_request(self, n, angle):
-        angle = angle*11.1 + 500
-        request = "#%sP%sT600\n" %(n, int(angle))
-        print request
-        self.serial.write(request)
-
     def motor_callback(self, data):
-        q = data.data.split('&')       
+        q = data.data.split('&')
         n = int(q[0][2:])
         a = int(q[1][2:])
-        print n, a
-        self.send_request(n, a)
+        self.motor_control(n, a)
+
+    def motor_control(self, n, angle):
+        def angle_to_pwm(angle):
+            return int(angle * 11.1 + 500)
+
+        request = "#%sP%sT600\n" % (n, angle_to_pwm(angle))
+        self.serial.write(request)
+        rospy.loginfo(request)
+
 
 if __name__ == '__main__':
     rospy.init_node('driver')
     driver = Driver('/dev/ttyUSB0', 9600)
     rospy.spin()
-
-
